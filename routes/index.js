cb4a1a66310cc1ef6999be32df17d379a1c2d9c8
phan-thanh <42335993+phanthethanh@users.noreply.github.com>
2018-08-14 16:48:07 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,18 @@
 var express = require('express');
 var router = express.Router();
 var Product = require('../models/products');
+var csrf = require('csurf');
+var csrfProtection = csrf();
 
+router.use(csrfProtection);
 
-/*Home page*/
 router.get('/', function(req,res,next){
-  var products = Product.find();
-
-  res.render('shop/index',{title: 'Shoes Shop', productList: products});
+  var products = Product.find(function(err, docs){
+    if (err) throw err;
+    res.render('shop/index',{title: 'Shoes Shop', products: docs});
+  });
 });
-
+router.get('/user/signin', function(req,res,next){
+  res.render('user/signin',{csrfToken: req.csrfToken()});
+});
 module.exports = router;
